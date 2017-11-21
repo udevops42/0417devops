d3453a572d38864a287ff06624eed6cd5068d3e9
uday7g <uday7g@users.noreply.github.com>
2017-11-20 21:50:41 -0600

    Update firstpython.py

--- python/firstpython.py
+++ python/firstpython.py
@@ -1,4 +1,4 @@
 import boto
 import boto.ec2
-conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='', aws_secret_access_key='')
-conn.run_instances('ami-8ca83fec', user_data='echo "Hello world"', instance_type='t2.micro')
+conn = boto.ec2.connect_to_region()
+conn.run_instances('ami-c998b6b2', user_data='echo "Hello world"', instance_type='t2.micro')
