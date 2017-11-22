395ef22c8a6b360ef2edfac0f4e46143c023a423
uday7g <uday7g@users.noreply.github.com>
2017-11-21 21:23:33 -0600

    Update firstpython.py

--- python/firstpython.py
+++ python/firstpython.py
@@ -1,4 +1,5 @@
 import boto
 import boto.ec2
-conn = boto.ec2.connect_to_region()
-conn.run_instances('ami-c998b6b2', user_data='echo "Hello world"', instance_type='t2.micro')
+conn = boto.ec2.connect_to_region("us-east-1")
+response=conn.run_instances('ami-c998b6b2', user_data='echo "Hello world"', instance_type='t2.micro')
+print response
